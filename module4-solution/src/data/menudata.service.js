857caea3abce460963b9f95fe5db73db187197d1
Ilmir Bakirov <ilmirbakirov@gmail.com>
2018-02-20 18:37:05 +0500

    refactoring base path

--- module4-solution/src/data/menudata.service.js
+++ module4-solution/src/data/menudata.service.js
@@ -1,34 +1,41 @@
-(function () {
-    'use strict';
+(function() {
+  'use strict';
 
-    angular.module('data')
-        .service('MenuDataService', MenuDataService);
+  angular.module('data')
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+    .service('MenuDataService', MenuDataService);
 
 
-    MenuDataService.$inject = ['$http'];
+  MenuDataService.$inject = ['$http', 'ApiBasePath'];
 
-    function MenuDataService($http) {
-        var service = this;
+  function MenuDataService($http, ApiBasePath) {
+    var service = this;
 
-        // this method should return a promise which is a result of using the $http service,
-        // using the following REST API endpoint: https://davids-restaurant.herokuapp.com/categories.json
-        service.getAllCategories = function () {
-            return $http.get('https://davids-restaurant.herokuapp.com/categories.json')
-                .then(function (response) {
-                    return response.data;
-                });
-        };
+    // this method should return a promise which is a result of using the $http service,
+    // using the following REST API endpoint: https://davids-restaurant.herokuapp.com/categories.json
+    service.getAllCategories = function() {
+      return $http.get(ApiBasePath + "/categories.json")
+        .then(function(response) {
+          return response.data;
+        });
+    };
 
-        // this method should return a promise which is a result of using the $http service,
-        // using the following REST API endpoint: https://davids-restaurant.herokuapp.com/menu_items.json?category=,
-        // where, before the call to the server, your code should append whatever categoryShortName value
-        // was passed in as an argument into the getItemsForCategory method.
-        service.getItemsForCategory = function (categoryShortName) {
-            return $http.get('https://davids-restaurant.herokuapp.com/menu_items.json?category=' + categoryShortName)
-                .then(function (response) {
-                    return response.data;
-                });
-        };
-    }
+    // this method should return a promise which is a result of using the $http service,
+    // using the following REST API endpoint: https://davids-restaurant.herokuapp.com/menu_items.json?category=,
+    // where, before the call to the server, your code should append whatever categoryShortName value
+    // was passed in as an argument into the getItemsForCategory method.
+    service.getItemsForCategory = function(shortName) {
+      return $http({
+          method: "GET",
+          url: (ApiBasePath + "/menu_items.json"),
+          params: {
+            category: shortName
+          }
+        })
+        .then(function(response) {
+          return response.data;
+        });
+    };
+  }
 
 })();
